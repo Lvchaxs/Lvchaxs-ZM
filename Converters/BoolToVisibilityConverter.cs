2f7540d7fe757b37c095a497a62b7461874778fe
伴生Lvchaxs <2163239604@qq.com>
2025-12-29 15:10:21 +0800

    Add files via upload

---
+++ Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿// Converters/BoolToVisibilityConverter.cs
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Lvchaxs_ZH.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public bool Invert { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool boolValue = value is bool b && b;
+            if (Invert) boolValue = !boolValue;
+            return boolValue ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
